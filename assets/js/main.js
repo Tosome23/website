211c115f720b129ee61ceeb0dd6b43e3386f1040
Tosome23 <23logis@naver.com>
2025-06-25 17:17:26 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,16 +254,5 @@
 			});
 
 		}
-document.addEventListener("DOMContentLoaded", function () {
-  const slides = document.querySelectorAll(".slide");
-  let current = 0;
-  slides[current].classList.add("active");
-
-  setInterval(() => {
-    slides[current].classList.remove("active");
-    current = (current + 1) % slides.length;
-    slides[current].classList.add("active");
-  }, 4000);
-});
 
 })(jQuery);
