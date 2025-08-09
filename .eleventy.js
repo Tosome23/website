54008604effd20926fb38924b2c89683df01819c
Tosome23 <23logis@naver.com>
2025-08-09 18:23:36 +0900

    .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,19 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-  // /blog 정적자산 복사
   eleventyConfig.addPassthroughCopy({ "blog/static/style.css": "blog/style.css" });
   eleventyConfig.addPassthroughCopy({ "blog/images": "blog/images" });
 
-  // 날짜 필터
-  eleventyConfig.addFilter("date", d => DateTime.fromJSDate(d).toFormat("yyyy-MM-dd"));
+  eleventyConfig.addFilter("date", d =>
+    DateTime.fromJSDate(d).toFormat("yyyy-MM-dd")
+  );
 
-  // 블로그 포스트 수집 (blog/content/blog/*.md)
-  eleventyConfig.addCollection("posts", (col) =>
+  eleventyConfig.addCollection("posts", col =>
     col.getFilteredByGlob("blog/content/blog/*.md")
   );
 
-  // 입력=루트(.), 레이아웃 폴더=blog/layouts, 출력=_site
   return {
-    dir: { input: ".", includes: "blog/layouts", output: "_site" },
+    dir: { input: ".", includes: "blog/layouts", output: "_site" }, // ★ 이것
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     templateFormats: ["md","njk","html"]
